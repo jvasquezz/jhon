aef6b19732cfb7cdfbc4393a79df2eaa4c83021e
Jhon Vasquez <jvasquez@unomaha.edu>
2017-12-19 12:04:00 -0500

    added content

--- src/app/images.ts
+++ src/app/images.ts
@@ -1,8 +1,8 @@
 import { Image } from './image';
 
 export const IMAGES: Image[] = [
-    { name: 'celaque', path: '../../assets/items1.png' },
-    { name: 'celaque', path: '../../assets/celaque.JPG' },
-    { name: 'atitlan', path: '../../assets/atitlan.jpeg' },
-    { name: 'celaque', path: '../../assets/mixed-forest.png' },
+    { name: 'celaque', path: 'assets/items1.png' },
+    { name: 'celaque', path: 'assets/celaque.JPG' },
+    { name: 'atitlan', path: 'assets/atitlan.jpeg' },
+    { name: 'celaque', path: 'assets/mixed-forest.png' },
 ];
