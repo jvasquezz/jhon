62c23ae0630ce40896431291302e3a6182afa1e8
Jhon Vasquez <jvasquez@unomaha.edu>
2017-12-19 09:41:02 -0500

    added styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Jhonatan Vasquez';
+  name = 'Jhonatan';
+  last = 'Vasquez';
 }
