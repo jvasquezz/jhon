1b5c90bf5d0bbed90ec40d6f49dc9913b18a40c5
Jhon Vasquez <jvasquez@unomaha.edu>
2017-12-20 02:23:42 -0500

    added vote buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name = 'Jhonatan';
   last = 'Vasquez';
+
+  votes = 1000;
+  // ngOnInit() {
+  //   this.votes = 0;
+  // }
+
+  addVoteUp(): void {
+    this.votes = this.votes + 1;
+    // console.log(this.votes);
+  }
 }
