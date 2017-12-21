fd28ac5413d1ef6de9cbe297437e90c9c4cd11c4
Jhon Vasquez <jvasquez@unomaha.edu>
2017-12-20 23:37:24 -0500

    added transition of top nav bar color when scrolling down

--- src/app/social/social.component.ts
+++ src/app/social/social.component.ts
@@ -6,13 +6,33 @@ import { SocialService } from '../social.service';
 @Component({
   selector: 'app-social',
   templateUrl: './social.component.html',
-  styleUrls: ['./social.component.css']
+  styleUrls: ['./social.component.css'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    '(window:scroll)': 'updateNavBar($event)'
+  }
 })
 export class SocialComponent implements OnInit {
 
   social_sites: Website[];
 
   constructor(private socialService: SocialService) { }
+
+  isScrolled = false;
+  currPos: Number = 0;
+  startPos: Number = 0;
+  changePos: Number = 50;
+
+  updateNavBar(evt) {
+    // console.log(this.currPos);
+    this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
+    if ( this.currPos >= this.changePos ) {
+        this.isScrolled = true;
+    } else {
+        this.isScrolled = false;
+    }
+    // console.log(this.isScrolled);
+  }
 
   ngOnInit() {
     this.getWebsites();
