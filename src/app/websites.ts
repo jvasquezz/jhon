c5df6271917fd91c4a996333bfb2ac5cc01080ac
Jhon Vasquez <jvasquez@unomaha.edu>
2017-12-30 18:56:23 -0500

    added navigation links to nav bar

--- src/app/websites.ts
+++ src/app/websites.ts
@@ -3,5 +3,6 @@ import { Website } from './website';
 export const WEBSITES: Website[] = [
     { domain_name: 'Quora', url: 'https://quora.com/profile/jon-vasco', icon: 'fa fa-quora' },
     { domain_name: 'Github', url: 'https://github.com/jonadiazz', icon: 'fa fa-github' },
-    { domain_name: 'LinkedIn', url: 'https://linkedin.com/in/jonadiazz/', icon: 'fa fa-linkedin' }
+    { domain_name: 'LinkedIn', url: 'https://linkedin.com/in/jonadiazz/', icon: 'fa fa-linkedin' },
+    { domain_name: 'Stack Overflow', url: 'https://stackoverflow.com/story/jvasquez', icon: 'fa fa-stack-overflow' }
 ];
